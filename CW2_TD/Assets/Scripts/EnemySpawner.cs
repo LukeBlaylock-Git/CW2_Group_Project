a6b8383f30e83145293311394069336a7878066e
LancashireladOnGitHub <lukeblaylock35@gmail.com>
2025-12-05 09:43:23 +0000

    Spline Pathfinding, Enemy AI, Tables
    Theres alot going on here
    
    First, we have the "Spline" thats the route our enemies will be taking.
    
    Next we got a "Blueprint" for all enemies, pressing right click in your content draw you will see the tab "TD" allowing you to create a new enemy using said blueprint
    
    And then the enemy spawner, which passes off information, stats, spline data to the enemy when they spawn in.

---
+++ CW2_TD/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;         // enemy prefab
+    public EnemyData EnemyType;            // enemy stats
+    public SplineContainer SplinePath;     // spline IN THE SCENE
+
+    public float SpawnInterval = 2f;
+    private float Timer = 0f;
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer >= SpawnInterval)
+        {
+            Timer = 0f;
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        // Spawn one enemy
+        GameObject e = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+
+        // Get the movement component
+        EnemySplineMovement move = e.GetComponent<EnemySplineMovement>();
+
+        // Assign data + spline
+        move.Type = EnemyType;
+        move.Path = SplinePath;
+    }
+}
---
+++ CW2_TD/Assets/Scripts/EnemySplineMover.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+
+public class EnemySplineMovement : MonoBehaviour
+{
+    public EnemyData Type; //Imported stats
+    public SplineContainer Path; //Spline holder, tldr just put the spline in here
+
+    private float Pos = 0f; //Current position along said Spline
+
+    void Start()
+    {
+        InitVisual();
+    }
+
+    void Update()
+    {
+        if (Path == null || Path.Splines.Count == 0) return; //If there is no path asigned or the spline has no curves, come to a stop
+
+        var spline = Path.Splines[0]; //Grab the first spline available (most levels SHOULD only have 1, if you have added anymore, ill be concerned.)
+
+       
+        float Length = SplineUtility.CalculateLength(spline, Path.transform.localToWorldMatrix); //Calculates the length of the spline.
+        if (Length < 0.01f) return;   // No length, no movement.
+
+        float dt = (Type.MoveSpeed / Length) * Time.deltaTime;
+        Pos += dt;
+        if (Pos >= 1f) //"if we had reached the end of the path"
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 localPos = SplineUtility.EvaluatePosition(spline, Pos);
+        Vector3 worldPos = Path.transform.TransformPoint(localPos);
+        Vector3 localTan = SplineUtility.EvaluateTangent(spline, Pos);
+        Vector3 worldTan = Path.transform.TransformDirection(localTan);
+
+        transform.position = worldPos;
+        if (worldTan.sqrMagnitude > 0.0001f)
+            transform.rotation = Quaternion.LookRotation(worldTan);
+    }
+
+    void InitVisual()
+    {
+        if (Type != null && Type.EnemyPrefab != null)
+        {
+            var obj = Instantiate(Type.EnemyPrefab, transform);
+            obj.transform.localPosition = Vector3.zero;
+        }
+    }
+}
